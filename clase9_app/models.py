1d48178aa54fdcf4ccad57b1a6be6b9a8469a33b
pablormacia <pr.macia@gmail.com>
2026-06-01 16:13:31 -0300

    Parte 3 - Modelando Productos y fixes

--- clase9_app/models.py
+++ clase9_app/models.py
@@ -1,7 +1,10 @@
+# models.py
 from django.db import models
 
-# Create your models here.
-class PlaygroundItem(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField(blank=True)
+    precio = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.nombre
--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'clase9_app',
 ]
 
 MIDDLEWARE = [
